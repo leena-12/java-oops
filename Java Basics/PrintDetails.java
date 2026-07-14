39f9cc94225f763dbf2bfa0d056224938568ccbc
Leena Kinkar <leenakinkar0912@gmail.com>
2026-07-14 21:47:09 +0530

    java basics

---
+++ Java Basics/PrintDetails.java
@@ -0,0 +1,7 @@
+public class PrintDetails {
+    public static void main (String[] args){
+        System.out.println("Name: Leena Kinkar");
+        System.out.println("Branch: CSE");
+        System.out.println("Favorite Programming Language: Python");
+    }
+}
