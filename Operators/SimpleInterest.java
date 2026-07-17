7f3267b58a8e9a354304b2d1cbe097b9f5fe7a99
Leena Kinkar <leenakinkar0912@gmail.com>
2026-07-17 23:07:17 +0530

    Operators(calculators)

---
+++ Operators/SimpleInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter rate: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter time: ");
+        double time = sc.nextDouble();
+
+        double simpleInterest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + simpleInterest);
+        sc.close();
+    }
+
+}
